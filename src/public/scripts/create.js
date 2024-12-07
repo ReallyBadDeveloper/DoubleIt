39d4245bc13aceb4c71d63b09b7cca7fc8c7add0
Really Bad Dev <reallybaddeveloper@gmail.com>
2024-12-07 17:33:22 -0500

    pushie

--- src/public/scripts/create.js
+++ src/public/scripts/create.js
@@ -11,6 +11,7 @@ document.querySelector('#create').addEventListener('click',() => {
 })
 
 var imgInput = document.querySelector('#img-select')
-imgInput.addEventListener('change', () => {
-    
+imgInput.addEventListener('change', (event) => {
+    document.querySelector('.select-img').replaceChildren()
+    document.querySelector('.select-img').style.background = URL.createObjectURL(imgInput.files[0])
 })

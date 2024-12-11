69e7dc023aba7ba51a60884278f201eb032dab01
Benjamin M. <reallybaddeveloper@gmail.com>
2024-12-11 18:52:53 +0000

    merge conflicts suck

--- src/public/scripts/thread.js
+++ src/public/scripts/thread.js
@@ -1,14 +1,20 @@
-function getData() {
+async function getThreadData() {
     var splitURL = window.location.href.split('/')
     document.querySelector('.card').innerText = splitURL[splitURL.length-1]
     const url = "/api/v1/thread/" + splitURL[splitURL.length-1];
-    fetch(url, { 
-        method: 'GET'
-      })
-      .then(function(response) { return response.json(); })
-      .then(function(json) {
-        console.log(json)
-    });
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        document.querySelector('.card').innerText = JSON.stringify(json);
+        return json;
+    } catch (error) {
+        console.error(error.message);
+        return;
+    }
 }
 
-document.querySelector('.card').innerText = JSON.stringify(getData())
+getThreadData()
